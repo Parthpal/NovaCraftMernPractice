9140ca1722e2bef6d2c2ef95936ead6620f54ed1
unknown <parthapradippal@gmail.com>
2025-04-28 21:51:27 +0530

    Homepage added

--- HomePageFeature.jsx
+++ HomePageFeature.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePageFeature = () => {
     return (
         <div>
-            <h1>HomePage nreqw featre</h1>
+            <h1>HomePage new featre</h1>
         </div>
     );
 };
