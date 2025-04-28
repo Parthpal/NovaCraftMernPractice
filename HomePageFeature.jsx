fe1d4c8cbf317fcd9b6dd05ef5954e75ef476101
unknown <parthapradippal@gmail.com>
2025-04-28 21:13:21 +0530

    HomeOranize service updated

--- HomePageFeature.jsx
+++ HomePageFeature.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePageFeature = () => {
     return (
         <div>
-            <h1>HomePage Feature</h1>
+            <h1>HomePage Feature,new</h1>
         </div>
     );
 };
